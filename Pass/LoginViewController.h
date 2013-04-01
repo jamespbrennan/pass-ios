de20e9b53a6aea49a1ab177147a06674d5c19419
James Brennan <james@jamesbrennan.ca>
2013-04-01 11:47:50 -0700

    Added objective c gitignore

---
+++ Pass/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Pass
+//
+//  Created by James Brennan on 2013-03-27.
+//  Copyright (c) 2013 PassAuth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Pass/LoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.m
+//  Pass
+//
+//  Created by James Brennan on 2013-03-27.
+//  Copyright (c) 2013 PassAuth. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
