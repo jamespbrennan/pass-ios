1a915fdce2b9c291de01805441c591b87cd25483
James Brennan <james@jamesbrennan.ca>
2013-04-12 13:22:37 -0700

    Working away at scanning. Done authenticate, working on register

--- Pass/ScanViewController.m
+++ Pass/ScanViewController.m
@@ -59,7 +59,7 @@
         
         NSArray *chunks = [result.text componentsSeparatedByString: @":"];
         
-        FMDatabase *db = ((AppDelegate *)[UIApplication sharedApplication].delegate).db;
+        
 
         if(chunks.count < 3)
         {
@@ -67,24 +67,26 @@
             int sessionId = [chunks[0] intValue];
             int serviceId = [chunks[1] intValue];
             NSString *token = chunks[2];
-            NSString *keyName;
+            NSString *keyName = [self getKeyName:serviceId];
             
-            // Find if we're registered to the service
-            FMResultSet *s = [db executeQuery:@"SELECT key_name FROM services WHERE id = (?) LIMIT 1", [NSNumber numberWithInteger:serviceId]];
-            if ([s next]) {
-                keyName = [s stringForColumn:@"key_name"];
+            if(keyName)
+            {
                 [self authenticate:token serviceId:serviceId sessionId:sessionId keyName:keyName];
-            } else {
-                
             }
-            
+            else
+            {
+                // Register first, then authenticate
+                keyName = [self register:serviceId];
+                [self authenticate:token serviceId:serviceId sessionId:sessionId keyName:keyName];
+            }
             
         }
     }
 }
 
-- (bool)register:(NSString*)token serviceId:(int)serviceId sessionId:(int)sessionId
+- (NSString*)register:(int)serviceId
 {
+    
     return NO;
 }
 
@@ -95,57 +97,79 @@
     
     if( [privateKey isEqualToString:@""] )
     {
-        return NO;
+        // If theres no private key available, register again
+        keyName = [self register:serviceId];
+        
+        keychainLookup = [NSString stringWithFormat:@"PrivateKey%@", keyName];
+        wrapper = [[KeychainItemWrapper alloc] initWithIdentifier:keychainLookup accessGroup:nil];
+        privateKey = [wrapper objectForKey:(id)CFBridgingRelease(kSecValueData)];
     }
-    else
+    
+    // Create a keypair for the service
+    JBRSA *rsa = [[JBRSA alloc] initWithPrivateKey:privateKey];
+    
+    NSString *post = [[NSString alloc] initWithFormat:@"token=%@&session_id=%d",[rsa privateEncrypt:token],sessionId];
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.passauth.net/sessions/authenticate"];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:url];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:postData];
+    
+    NSError *error = [[NSError alloc] init];
+    NSHTTPURLResponse *response = nil;
+    NSData *urlData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+    
+    SBJsonParser *jsonParser = [SBJsonParser new];
+    NSDictionary *jsonData = (NSDictionary *) [jsonParser objectWithString:responseData];
+    
+    if ([response statusCode] == 200)
     {
-        // Create a keypair for the service
-        JBRSA *rsa = [[JBRSA alloc] initWithPrivateKey:privateKey];
-        
-        NSString *post = [[NSString alloc] initWithFormat:@"token=%@&session_id=%d",[rsa privateEncrypt:token],sessionId];
-        NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-        NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
-        
-        NSURL *url = [NSURL URLWithString:@"https://api.passauth.net/sessions/authenticate"];
-        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-        [request setURL:url];
-        [request setHTTPMethod:@"POST"];
-        [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-        [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-        [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-        [request setHTTPBody:postData];
-        
-        NSError *error = [[NSError alloc] init];
-        NSHTTPURLResponse *response = nil;
-        NSData *urlData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-        
-        NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
-        
-        SBJsonParser *jsonParser = [SBJsonParser new];
-        NSDictionary *jsonData = (NSDictionary *) [jsonParser objectWithString:responseData];
-        
-        if ([response statusCode] == 200)
+        if ((bool) [jsonData objectForKey:@"is_authenticated"])
         {
-            if ((bool) [jsonData objectForKey:@"is_authenticated"])
-            {
-                [self alertStatus:@"The login screen should refresh any moment now." :@"Successful Login"];
-                return YES;
-            }
-            else
-            {
-                [self alertStatus:@"Sorry, we weren't able to successfully authenticate you. Please try again." :@"Error"];
-            }
+            [self alertStatus:@"The login screen should refresh any moment now." :@"Successful Login"];
+            return YES;
         }
         else
         {
-            // Log the NSURLConnection error, if any
-            if (error) NSLog(@"Login error: %@", error);
-            
-            NSString *message = [jsonData objectForKey:@"error[message]"];
-            [self alertStatus:message :@"Error"];
+            [self alertStatus:@"Sorry, we weren't able to successfully authenticate you. Please try logging in again." :@"Error"];
         }
+    }
+    else
+    {
+        // Log the NSURLConnection error, if any
+        if (error) NSLog(@"Login error: %@", error);
         
-        return NO;
+        NSString *message = [jsonData objectForKey:@"error[message]"];
+        NSLog(@"Login error: %@", message);
+        [self alertStatus: @"Sorry, something went wrong on our end. Please try logging in again." :@"Error"];
+    }
+    
+    return NO;
+}
+
+- (bool) setServiceKeyName:(int)serviceId keyName:(NSString*)keyName
+{
+    FMDatabase *db = ((AppDelegate *)[UIApplication sharedApplication].delegate).db;
+    return [db executeQuery:@"INSERT OR REPLACE INTO services (id, key_name) VALUES(?, ?)", [NSNumber numberWithInteger:serviceId], keyName];
+}
+
+- (NSString*) getServiceKeyName:(int)serviceId
+{
+    FMDatabase *db = ((AppDelegate *)[UIApplication sharedApplication].delegate).db;
+    // Find if we're registered to the service
+    FMResultSet *s = [db executeQuery:@"SELECT key_name FROM services WHERE id = (?) LIMIT 1", [NSNumber numberWithInteger:serviceId]];
+    if ([s next]) {
+        return [s stringForColumn:@"key_name"];
+    } else {
+        return nil;
     }
 }
 
